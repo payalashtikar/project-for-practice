22fd2af6f09d0b4d7fed9ab58b515e0ec4a1744a
ashtikar_payal <75967067+payalashtikar@users.noreply.github.com>
2023-01-18 17:19:02 +0530

    formDataRoute created with post get & del cmd

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose')
 const { DB } = require('./key.js')
 const port = 5010;
+const cors = require('cors')
+require('./model/formDataModel')
 
 const connectionParams = {
     useNewUrlParser: true,
@@ -17,7 +19,11 @@ mongoose
         console.log("DB connection failed", err);
     });
 
-    
+app.use(express.json())
+app.use(cors());
+
+app.use(require('./routes/formDataRoute'))
+
 app.listen(port, () => {
     console.log(`server listening at ${port}`)
 })
---
+++ server/routes/formDataRoute.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const FormData = mongoose.model('FormDataModel')
+const router = express.Router();
+
+
+router.post('/createuserformdata', async (req, res) => {
+    try {
+        const { firstName, lastName, address, gender, age, city, status, email, contact, profilePhoto, Date } = req.body;
+        if (!firstName || !lastName || !address || !gender || !age || !city || !status || !email || !contact || !profilePhoto || !Date) {
+            return res.status(400).json({ error: "please fill all required fields !!" })
+        }
+        const postData = new FormData({
+            firstName,
+            lastName,
+            address,
+            gender,
+            age,
+            city,
+            status,
+            email,
+            contact,
+            profilePhoto,
+            Date,
+            ...req.body,
+        })
+        await postData.save();
+        console.log(postData)
+        return res.status(200).json({ message: "User Data Created", postData })
+    }
+    catch (err) {
+        return res.status(400).json({ error: err })
+    }
+});
+
+
+router.get('/getalluserdata', async (req, res) => {
+    try {
+        const { firstName, lastName, address, gender, age, city, status, email, contact, profilePhoto, Date } = req.body;
+        const userData = await FormData.find({})
+        console.log("congo...you have got users data")
+        return res.status(200).json({ message: "you have got users data", userData })
+    }
+    catch (err) {
+        console.log("you have'nt got users data")
+        return res.status(400).json({ error: err })
+    }
+})
+
+router.delete('/removeusersdata/:id' , async(req,res)=>{
+    let removeData = await FormData.findByIdAndDelete({_id : req.params.id})
+    console.log(`${req.params.id} , users data has been deleted`)
+    return res.status(200).json({ message: " users data has been deleted", removeData })
+})
+
+module.exports = router;
+
